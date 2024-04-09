38a9d72b2e9b515468c5c14cbd287682d85c7e82
phucduongminh <phucbopbnation@gmail.com>
2024-04-09 17:07:12 +0700

    solu

--- CSP header config/Wrapper.jsx
+++ CSP header config/Wrapper.jsx
@@ -1,8 +1,18 @@
 import App from "../App";
 import React from "react";
+import createCache from '@emotion/cache';
+import { CacheProvider } from "@emotion/react";
 
 const Wrapper = () => {
-    return <App />;
+    const cache = createCache({
+        nonce: "__GENERATED_NONCE__",
+        key: "boatinsurances"
+    })
+    return (
+        <CacheProvider value={cache}>
+            <App />
+        </CacheProvider>
+    );
 }
 
 export default Wrapper;
